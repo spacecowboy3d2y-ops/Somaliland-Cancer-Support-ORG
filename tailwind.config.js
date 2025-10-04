32096e0b2d732eaf98735a31d00651cd4e4b69c4
spacecowboy3d2y-ops <spacecowboy3d2y@gmail.com>
2025-10-04 15:23:37 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-purple': '#A076A8',
+        'brand-light-purple': '#F3E8F5',
+        'brand-dark': '#333333',
+      }
+    },
+  },
+  plugins: [],
+}
